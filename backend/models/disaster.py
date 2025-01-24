f453a6c01a773dce10d6dcbff5638d3ad4015bbd
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-01-24 21:19:28 +0000

    feat: something working so far

--- backend/models/disaster.py
+++ backend/models/disaster.py
@@ -1,28 +1,29 @@
 class DisasterFactory():
-    def create_disaster(self, disaster_type, name, longitude, latitude, radius_km):
+    def create_disaster(self, disaster_type, name, description, longitude, latitude, radius_km):
         if disaster_type == 'natural':
-            return DisasterNatural(name, longitude, latitude, radius_km)
+            return DisasterNatural(name, description, longitude, latitude, radius_km)
         elif disaster_type == 'biological':
-            return DisasterBiological(name, longitude, latitude, radius_km)
+            return DisasterBiological(name, description, longitude, latitude, radius_km)
         elif disaster_type == 'manmade':
-            return DisasterManMade(name, longitude, latitude, radius_km)
+            return DisasterManMade(name, description, longitude, latitude, radius_km)
 
 class Disaster():
-    def __init__(self, name, longitude, latitude, radius_km):
+    def __init__(self, name, description, longitude, latitude, radius_km):
         self.name = name
+        self.description = description
         self.longitude = longitude
         self.latitude = latitude
         self.radius_km = radius_km
 
 class DisasterNatural(Disaster):
-    def __init__(self, name, longitude, latitude, radius_km):
-        super().__init__(name, longitude, latitude, radius_km)
+    def __init__(self, name, description, longitude, latitude, radius_km):
+        super().__init__(name, description, longitude, latitude, radius_km)
         
 
 class DisasterBiological(Disaster):
-    def __init__(self, name, longitude, latitude, radius_km):
-        super().__init__(name, longitude, latitude, radius_km)
+    def __init__(self, name, description, longitude, latitude, radius_km):
+        super().__init__(name, description, longitude, latitude, radius_km)
 
 class DisasterManMade(Disaster):
-    def __init__(self, name, longitude, latitude, radius_km):
-        super().__init__(name, longitude, latitude, radius_km)
+    def __init__(self, name, description, longitude, latitude, radius_km):
+        super().__init__(name, description, longitude, latitude, radius_km)
--- backend/subhub.py
+++ backend/subhub.py
@@ -1,4 +1,6 @@
-# python3 subhub.py --hub-host 127.0.0.1 --hub-port 3000 --host 0.0.0.0 --port 3001 --id 0 --name Hamitlon --longitude 43.26691127633942 --latitude -79.77373983912754 --radius_km 1183.31
+# python3 subhub.py --hub-host 127.0.0.1 --hub-port 3000 --ip 40.233.92.183 --host 0.0.0.0 --port 3001 --id 0 --name Hamitlon --longitude 43.26691127633942 --latitude -79.77373983912754 --radius_km 1183.31
+
+# python3 subhub.py --hub-host 127.0.0.1 --hub-port 3000 --ip 40.233.92.183 --host 0.0.0.0 --port 3002 --id 1 --name Burlington --longitude 43.3255 --latitude 79.7990 --radius_km 1183.31
 import argparse
 import requests
 from flask import Flask, jsonify, send_file, request
@@ -9,7 +11,7 @@ app = Flask(__name__)
 CORS(app)
 
 # this function subscribes this subhub to a primary hub
-def subscribe_to_hub(hub_host: str, hub_port: int, host: str, port: int, id: int, name: str, longitude: float, latitude: float, radius_km: float):
+def subscribe_to_hub(hub_host: str, hub_port: int, ip: str, port: int, id: int, name: str, longitude: float, latitude: float, radius_km: float):
     # curl http://40.233.92.183:3000/subscribe_to_hub?id=3&name=Kitchener&longitude=43.4504&latitude=80.4832&radius_km=100
     # make API call
     # parameters are id, name, longitude, latitude, radius_km
@@ -19,7 +21,7 @@ def subscribe_to_hub(hub_host: str, hub_port: int, host: str, port: int, id: int
     # parameters are id, name, longitude, latitude, radius_km
     url = f"http://{hub_host}:{hub_port}/subscribe_to_hub"
     payload = {
-        'host': host,
+        'ip': ip,
         'port': port,
         'id': id,
         'name': name,
@@ -27,6 +29,7 @@ def subscribe_to_hub(hub_host: str, hub_port: int, host: str, port: int, id: int
         'latitude': latitude,
         'radius_km': radius_km
     }
+    print('Sending payload to hub: ', payload, 'at url: ', url)
     response = requests.post(url, data=payload)
     if response.status_code == 200:
         print("Successfully subscribed to hub")
@@ -35,15 +38,27 @@ def subscribe_to_hub(hub_host: str, hub_port: int, host: str, port: int, id: int
         print(f"Failed to subscribe to hub: {response.status_code}, {response.text}")
         return False
     
+# curl -X POST "http://40.233.92.183:3001/report
 @app.route('/report')
 def report():
     # parameters are disaster_type, name, longitude, latitude
+
+    # sucecss message
+    # {'name': 'Burlington', 'id': 0, 'longitude': 43.3255, 'latitude': 79.799}
+
+    
+    pass
+
+@app.route('/disasters')
+def disasters():
+    # get a list of disasters from this sub hub
     pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Sub Hub', description='Run a sub hub instance that users can report to')
     parser.add_argument('--hub-host', type=str, default='127.0.0.1', help='The host of the primary hub')
     parser.add_argument('--hub-port', type=int, default=3000, help='The port of the primary hub')
+    parser.add_argument('--ip', type=str, help='The ip of the sub hub that can be accessible and seen by the primary hub')
     parser.add_argument('--host', type=str, default='0.0.0.0', help='The host to run the sub hub on')
     parser.add_argument('--port', type=int, help='The port to run the sub hub on')
     parser.add_argument('--id', type=int, help='The id of the sub hub')
@@ -56,7 +71,7 @@ if __name__ == '__main__':
     success = subscribe_to_hub(
         hub_host=args.hub_host,
         hub_port=args.hub_port,
-        host=args.host,
+        ip=args.ip,
         port=args.port,
         id=args.id,
         name=args.name,
