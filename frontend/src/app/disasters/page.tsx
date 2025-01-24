0be5d97f432ec6c4370e8329b4dac3fc33c5e112
JerryShum <jerryshum2636@gmail.com>
2025-01-24 17:06:07 -0500

    make pages for forms

--- frontend/src/app/disasters/page.tsx
+++ frontend/src/app/disasters/page.tsx
@@ -1,5 +1,10 @@
+import EnterAddress from '@/components/forms/EnterAddress';
 import React from 'react';
 
 export default function FindDisastersPage() {
-   return <div>page</div>;
+   return (
+      <div className="mt-10 flex flex-col items-center">
+         <EnterAddress />
+      </div>
+   );
 }
--- frontend/src/app/report/page.tsx
+++ frontend/src/app/report/page.tsx
@@ -1,5 +1,11 @@
+import ReportDisaster from '@/components/forms/ReportDisaster';
 import React from 'react';
 
 export default function ReportDisastersPage() {
-   return <div>page</div>;
+   return (
+      <div>
+         <h1>Report a disaster</h1>
+         <ReportDisaster />
+      </div>
+   );
 }
